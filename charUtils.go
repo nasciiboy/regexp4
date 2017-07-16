c38f6156044f0110136f4b2f159fe4d867bcfd83
nasciiboy <nasciiboy@gmail.com>
2017-07-15 21:23:38 -0500

    =======================================================    Recursive Regexp Raptor (regexp4 go-lang port) v0.07 =======================================================    new file:   LICENSE.txt    new file:   charUtils.go    new file:   readme.org    new file:   readme_es.org    new file:   regexp4.go    new file:   regexp4_test.go ------------------------------------------------------- Sonic-Boom -------------------------------------------------------    Space Dandy : Boobies Wonderland    Kazu Matsui : Bamboo    Morcheeba   : Charango                : Who Can You Trust                : Big Calm -------------------------------------------------------

---
+++ charUtils.go
@@ -0,0 +1,160 @@
+package regexp4
+
+func isDigit( c rune ) bool { return c >= '0' && c <= '9' }
+func isUpper( c rune ) bool { return c >= 'a' && c <= 'z' }
+func isLower( c rune ) bool { return c >= 'A' && c <= 'Z' }
+func isAlpha( c rune ) bool { return isLower( c ) || isUpper( c ) }
+func isAlnum( c rune ) bool { return isAlpha( c ) || isDigit( c ) }
+func isSpace( c rune ) bool { return c == ' ' || (c >= '\t' && c <= '\r') }
+func isBlank( c rune ) bool { return c == ' ' || c == '\t' }
+
+func toLower( c rune ) rune {
+  if isLower( c ) { return c + 32 }
+
+  return c
+}
+
+func strChr( str string, r rune ) int {
+  for i, c := range str {
+    if c == r { return  i }
+  }
+
+  return -1
+}
+
+func strnchr( str string, v rune ) bool {
+  for _, c := range( str) {
+    if c == v { return true }
+  }
+
+  return false
+}
+
+func cmpChrCommunist( a, b rune ) bool {
+  return toLower( a ) == toLower( b )
+}
+
+func findRuneCommunist( str string, chr rune ) bool {
+  for _, c := range str {
+    if cmpChrCommunist( c, chr ) == false { return false }
+  }
+
+  return true;
+}
+
+func findRuneCommunisto( str string, chr rune ) bool {
+  for _, c := range str {
+    if toLower( c ) == toLower( chr ) { return true }
+  }
+
+  return false;
+}
+
+func strnEqlCommunist( s, t string, n int ) bool {
+  for i := 0; i < n; i++ {
+    if cmpChrCommunist( rune(s[i]), rune(t[i]) ) == false  { return false }
+  }
+
+  return true;
+}
+
+func aToi( str string ) ( number int ) {
+  for _, c := range str {
+    if isDigit( c ) == false { return }
+
+    number = 10 * number + ( int(c) - '0' )
+  }
+
+  return
+}
+
+func countCharDigits( str string ) int {
+  for i, c := range str {
+    if isDigit( c ) == false { return i }
+  }
+
+  return len( str )
+}
+
+//////////////////////  from github.com/golang/go/src/unicode/utf8 //////////////////////
+const (
+  t1 = 0x00   // 0000 0000
+  tx = 0x80   // 1000 0000
+  t2 = 0xC0   // 1100 0000
+  t3 = 0xE0   // 1110 0000
+  t4 = 0xF0   // 1111 0000
+  t5 = 0xF8   // 1111 1000
+
+  locb = 0x80 // 1000 0000
+  hicb = 0xBF // 1011 1111
+
+  xx = 0xF1   // invalid: size 1
+  as = 0xF0   // ASCII: size 1
+  s1 = 0x02   // accept 0, size 2
+  s2 = 0x13   // accept 1, size 3
+  s3 = 0x03   // accept 0, size 3
+  s4 = 0x23   // accept 2, size 3
+  s5 = 0x34   // accept 3, size 4
+  s6 = 0x04   // accept 0, size 4
+  s7 = 0x44   // accept 4, size 4
+)
+
+var first = [256]uint8{
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x00-0x0F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x10-0x1F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x20-0x2F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x30-0x3F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x40-0x4F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x50-0x5F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x60-0x6F
+  as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, as, // 0x70-0x7F
+  xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0x80-0x8F
+  xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0x90-0x9F
+  xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xA0-0xAF
+  xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xB0-0xBF
+  xx, xx, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, // 0xC0-0xCF
+  s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, s1, // 0xD0-0xDF
+  s2, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s3, s4, s3, s3, // 0xE0-0xEF
+  s5, s6, s6, s6, s7, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, xx, // 0xF0-0xFF
+}
+
+type acceptRange struct {
+  lo uint8 // lowest value for second byte.
+  hi uint8 // highest value for second byte.
+}
+
+var acceptRanges = [...]acceptRange{
+  0: {locb, hicb},
+  1: {0xA0, hicb},
+  2: {locb, 0x9F},
+  3: {0x90, hicb},
+  4: {locb, 0x8F},
+}
+
+func utf8meter(s string) int {
+  n := len(s)
+  if n < 1 { return 0 }
+
+  s0 := s[0]
+  x := first[s0]
+  if x >= as { return 1 }
+
+  sz := x & 7
+  accept := acceptRanges[x>>4]
+  if n < int(sz) { return 1 }
+
+  s1 := s[1]
+  if s1 < accept.lo || accept.hi < s1 { return 1 }
+
+  if sz == 2 { return 2 }
+
+  s2 := s[2]
+  if s2 < locb || hicb < s2 { return 1 }
+
+  if sz == 3 { return 3 }
+
+  s3 := s[3]
+  if s3 < locb || hicb < s3 { return 1 }
+
+  return 4
+}
---
+++ regexp4.go
@@ -0,0 +1,626 @@
+package regexp4
+
+const inf = 1073741824 // 2^30
+
+const (
+  modAlpha      uint8 = 1
+  modOmega      uint8 = 2
+  modLonley     uint8 = 4
+  modFwrByChar  uint8 = 8
+  modCommunism  uint8 = 16
+  modNegative   uint8 = 128
+  modPositive   uint8 = ^modNegative
+  modCapitalism uint8 = ^modCommunism
+)
+
+const ( rePath uint8 = iota; reGroup; reHook; reSet; reBackref; reMeta; reRangeab; reUTF8; rePoint; reSimple )
+
+type reStruct struct {
+  str                string
+  reType             uint8
+  mods               uint8
+  loopsMin, loopsMax int
+}
+
+type catchInfo struct {
+  init, end, id int
+}
+
+type raptorASM struct {
+  inst  uint8
+  close int
+  re    reStruct
+}
+
+const (
+  asmPathIni = iota; asmPathEle; asmPathEnd;
+  asmGroupIni; asmGroupEnd; asmHookIni; asmHookEnd; asmSetIni; asmSetEnd;
+  asmBackref; asmMeta; asmRangeab; asmUTF8; asmPoint; asmSimple; asmEnd
+)
+
+type RE struct {
+  txt, re      string
+  result       int
+
+  txtInit      int
+  txtLen       int
+  txtPos       int
+
+  catches      []catchInfo
+  catchIndex   int
+  catchIdIndex int
+
+  asm          []raptorASM
+  mods         uint8
+}
+
+func (r *RE) Compile( re string ){
+  rexp := reStruct{ str: re, reType: rePath }
+  r.re  = re
+  r.asm = make( []raptorASM, 0, 32 )
+
+  getMods( &rexp, &rexp )
+  r.mods = rexp.mods
+
+  if isPath( &rexp ) { r.genPaths( rexp )
+  } else { r.genTracks( &rexp ) }
+
+  r.asm = append( r.asm, raptorASM{ inst: asmEnd, close: len(r.asm) } )
+}
+
+func isPath( rexp *reStruct ) bool {
+  if len(rexp.str) == 0 { return false }
+
+  for i, deep := 0, 0; walkMeta( rexp.str[i:], &i ) < len( rexp.str ); i++ {
+    switch rexp.str[ i ] {
+    case '(', '<': deep++
+    case ')', '>': deep--
+    case '[': i += walkSet( rexp.str[i:] )
+    case '|': if deep == 0 { return true }
+    }
+  }
+
+  return false
+}
+
+func (r *RE) genPaths( rexp reStruct ) {
+  var track reStruct
+  pathIndex := len( r.asm )
+  r.asm = append( r.asm, raptorASM{ inst: asmPathIni, re: rexp } )
+
+  for cutByType( &rexp, &track, rePath ) {
+    trackIndex := len( r.asm )
+    r.asm = append( r.asm, raptorASM{ inst: asmPathEle, re: track } )
+    r.genTracks( &track )
+    r.asm[trackIndex].close = len( r.asm )
+  }
+
+  r.asm[pathIndex].close = len( r.asm )
+  r.asm = append( r.asm, raptorASM{ inst: asmPathEnd, close: len(r.asm) } )
+}
+
+func (r *RE) genTracks( rexp *reStruct ){
+  var track reStruct
+  for tracker( rexp, &track ) {
+    trackIndex := len( r.asm )
+    switch track.reType {
+    case reHook   :
+      r.asm = append( r.asm, raptorASM{ inst: asmHookIni, re: track } )
+
+      if isPath( &track ) { r.genPaths ( track )
+      } else              { r.genTracks( &track ) }
+
+      r.asm[trackIndex].close = len( r.asm )
+      r.asm = append( r.asm, raptorASM{ inst: asmHookEnd, close: len(r.asm) } )
+    case reGroup  :
+      r.asm = append( r.asm, raptorASM{ inst: asmGroupIni, re: track } )
+
+      if isPath( &track ) { r.genPaths ( track )
+      } else              { r.genTracks( &track ) }
+
+      r.asm[trackIndex].close = len( r.asm )
+      r.asm = append( r.asm, raptorASM{ inst: asmGroupEnd, close: len(r.asm) } )
+    case rePath   :
+    case reSet    : r.genSet( &track )
+    case reBackref: r.asm = append( r.asm, raptorASM{ inst: asmBackref, close: trackIndex, re: track } )
+    case reMeta   : r.asm = append( r.asm, raptorASM{ inst: asmMeta   , close: trackIndex, re: track } )
+    case reRangeab: r.asm = append( r.asm, raptorASM{ inst: asmRangeab, close: trackIndex, re: track } )
+    case reUTF8   : r.asm = append( r.asm, raptorASM{ inst: asmUTF8   , close: trackIndex, re: track } )
+    case rePoint  : r.asm = append( r.asm, raptorASM{ inst: asmPoint  , close: trackIndex, re: track } )
+    default       : r.asm = append( r.asm, raptorASM{ inst: asmSimple , close: trackIndex, re: track } )
+    }
+  }
+}
+
+func (r *RE) genSet( rexp *reStruct ){
+  if len(rexp.str) == 0 { return }
+
+  if rexp.str[0] == '^' {
+    rexp.str = rexp.str[1:]
+    if rexp.mods & modNegative > 0 { rexp.mods &= modPositive
+    } else                         { rexp.mods |= modNegative }
+  }
+
+  SetIndex := len( r.asm )
+  r.asm = append( r.asm, raptorASM{ inst: asmSetIni, re: *rexp } )
+
+  var track reStruct
+  for trackerSet( rexp, &track ) {
+    switch track.reType {
+    case reMeta   : r.asm = append( r.asm, raptorASM{ inst: asmMeta   , close: len(r.asm), re: track } )
+    case reRangeab: r.asm = append( r.asm, raptorASM{ inst: asmRangeab, close: len(r.asm), re: track } )
+    case reUTF8   : r.asm = append( r.asm, raptorASM{ inst: asmUTF8   , close: len(r.asm), re: track } )
+    default       : r.asm = append( r.asm, raptorASM{ inst: asmSimple , close: len(r.asm), re: track } )
+    }
+  }
+
+  r.asm[SetIndex].close = len( r.asm )
+  r.asm = append( r.asm, raptorASM{ inst: asmSetEnd, close: len(r.asm) } )
+}
+
+func trackerSet( rexp, track *reStruct ) bool {
+  if len( rexp.str ) == 0 { return false }
+
+  if rexp.str[0] > 127 {
+    cutByLen( rexp, track, utf8meter( rexp.str ), reUTF8 )
+  } else if rexp.str[0] == ':' {
+    cutByLen ( rexp, track, 2, reMeta  )
+  } else {
+    for i := 0; i < len( rexp.str ); i++ {
+      if rexp.str[i] > 127 {
+        cutByLen( rexp, track, i, reSimple  ); goto setLM;
+      } else {
+        switch rexp.str[i] {
+        case ':': cutByLen( rexp, track, i, reSimple  ); goto setLM;
+        case '-':
+          if i == 1 { cutByLen( rexp, track,     3, reRangeab )
+          } else    { cutByLen( rexp, track, i - 1, reSimple  ) }
+
+          goto setLM;
+        }
+      }
+    }
+
+    cutByLen( rexp, track, len( rexp.str ), reSimple  );
+  }
+
+ setLM:
+  track.loopsMin, track.loopsMax = 1, 1
+  track.mods &= modPositive
+  return true
+}
+
+func tracker( rexp, track *reStruct ) bool {
+  if len( rexp.str ) == 0 { return false }
+
+  if rexp.str[0] > 127 {
+    cutByLen( rexp, track, utf8meter( rexp.str ), reUTF8 )
+  } else {
+    switch rexp.str[0] {
+    case ':': cutByLen ( rexp, track, 2,     reMeta    )
+    case '.': cutByLen ( rexp, track, 1,     rePoint   )
+    case '@': cutByLen ( rexp, track, 1 +
+            countCharDigits( rexp.str[1:] ), reBackref )
+    case '(': cutByType( rexp, track,        reGroup   )
+    case '<': cutByType( rexp, track,        reHook    )
+    case '[': cutByType( rexp, track,        reSet     )
+    default : cutSimple( rexp, track                   )
+    }
+  }
+
+  getLoops( rexp, track );
+  getMods ( rexp, track );
+  return true
+}
+
+func cutSimple( rexp, track *reStruct ){
+  for i, c := range rexp.str {
+    if c > 127 {
+      cutByLen( rexp, track, i, reSimple  ); return
+    } else {
+      switch c {
+      case '(', '<', '[', '@', ':', '.':
+        cutByLen( rexp, track, i, reSimple  ); return
+      case '?', '+', '*', '{', '#':
+        if i == 1 { cutByLen( rexp, track,     1, reSimple  )
+        } else    { cutByLen( rexp, track, i - 1, reSimple  ) }
+        return
+      }
+    }
+  }
+
+  cutByLen( rexp, track, len(rexp.str), reSimple  );
+}
+
+func cutByLen( rexp, track *reStruct, length int, reType uint8 ){
+  *track       = *rexp
+  track.str    = rexp.str[:length]
+  rexp.str     = rexp.str[length:]
+  track.reType = reType;
+}
+
+func cutByType( rexp, track *reStruct, reType uint8 ) bool {
+  if len(rexp.str) == 0 { return false }
+
+  *track       = *rexp
+  track.reType = reType
+  for i , deep, cut := 0, 0, false; walkMeta( rexp.str[i:], &i ) < len( rexp.str ); i++ {
+    switch rexp.str[ i ] {
+    case '(', '<': deep++
+    case ')', '>': deep--
+    case '[': i += walkSet( rexp.str[i:] )
+    }
+
+    switch reType {
+    case reHook, reGroup: cut = deep == 0
+    case reSet          : cut = rexp.str[ i ] == ']'
+    case rePath         : cut = rexp.str[ i ] == '|' && deep == 0
+    }
+
+    if cut {
+      track.str  = rexp.str[:i]
+      rexp.str   = rexp.str[i + 1:]
+      if reType != rePath { track.str = track.str[1:] }
+      return true
+    }
+  }
+
+  rexp.str = ""
+  return true
+}
+
+func walkSet( str string ) int {
+  for i := 0; walkMeta( str[i:], &i ) < len( str ); i++ {
+    if str[i] == ']' { return i }
+  }
+
+  return len(str);
+}
+
+func walkMeta( str string, n *int ) int {
+  for i := 0; i < len( str ); i += 2 {
+    if str[i] != ':' { *n += i; return *n }
+  }
+
+  *n += len( str )
+  return *n
+}
+
+func getMods( rexp, track *reStruct ){
+  track.mods &= modPositive
+
+  if len( rexp.str ) > 0 && rexp.str[ 0 ] == '#' {
+    for i, c := range rexp.str[1:] {
+      switch c {
+      case '^': track.mods |= modAlpha
+      case '$': track.mods |= modOmega
+      case '?': track.mods |= modLonley
+      case '~': track.mods |= modFwrByChar
+      case '*': track.mods |= modCommunism
+      case '/': track.mods &= modCapitalism
+      case '!': track.mods |= modNegative
+      default : rexp.str    = rexp.str[i+1:]; return
+      }
+    }
+
+    rexp.str = ""
+  }
+}
+
+func getLoops( rexp, track *reStruct ){
+  pos := 0;
+  track.loopsMin, track.loopsMax = 1, 1
+
+  if len( rexp.str ) > 0 {
+    switch rexp.str[0] {
+    case '?' : pos = 1; track.loopsMin = 0; track.loopsMax =   1;
+    case '+' : pos = 1; track.loopsMin = 1; track.loopsMax = inf;
+    case '*' : pos = 1; track.loopsMin = 0; track.loopsMax = inf;
+    case '{' : pos = 1
+      track.loopsMin = aToi( rexp.str[pos:] )
+      pos += countCharDigits( rexp.str[pos:] )
+
+      if rexp.str[pos] == '}' {
+        track.loopsMax = track.loopsMin;
+        pos += 1
+      } else if rexp.str[pos:pos+2] == ",}" {
+        pos += 2
+        track.loopsMax = inf
+      } else if rexp.str[pos] == ',' {
+        pos += 1
+        track.loopsMax = aToi( rexp.str[pos:] )
+        pos += countCharDigits( rexp.str[pos:] ) + 1
+      }
+    }
+
+    rexp.str = rexp.str[pos:]
+  }
+}
+
+//-! match
+
+func (r *RE) MatchBool( txt, re string ) bool {
+  if r.Match( txt, re ) > 0 { return true }
+
+  return false
+}
+
+func (r *RE) Match( txt, re string ) int {
+  loops       := len(txt)
+  r.txt        = txt
+  r.result     = 0
+  r.catches    = make( []catchInfo, 32 )
+  r.catchIndex = 1
+  if len(txt) == 0 || len(re) == 0 { return 0 }
+
+  r.Compile( re )
+
+  if (r.mods & modAlpha) > 0 { loops = 1 }
+
+  for forward, i, ocindex := 0, 0, 0; i < loops; i += forward {
+    forward, r.catchIdIndex       = utf8meter( txt[i:] ), 1
+    r.txtPos, r.txtInit, r.txtLen = 0, i, len( txt[i:] )
+    ocindex                       = r.catchIndex
+
+    if r.trekking( 0 ) {
+      if (r.mods & modOmega) > 0 {
+        if r.txtPos == r.txtLen                                 { r.result = 1; return 1
+        } else { r.catchIndex = 1 }
+      } else if (r.mods & modLonley   ) > 0                  { r.result = 1; return 1
+      } else if (r.mods & modFwrByChar) > 0 || r.txtPos == 0 { r.result++
+      } else {   forward = r.txtPos;                              r.result++; }
+    } else { r.catchIndex = ocindex }
+  }
+
+  return r.result
+}
+
+func (r *RE) trekking( index int ) (result bool) {
+  switch r.asm[ index ].inst {
+  case asmEnd, asmPathEnd, asmPathEle, asmGroupEnd, asmHookEnd, asmSetEnd: return true
+  case asmHookIni :
+    iCatch := r.openCatch();
+    result  = r.loopGroup( index )
+    if result { r.closeCatch( iCatch ) }
+  case asmGroupIni: result = r.loopGroup( index )
+  case asmPathIni : result = r.walker   ( index )
+  default         : result = r.looper   ( index )
+  }
+
+  if result && r.trekking( r.asm[ index ].close + 1 ) { return true }
+
+  return false
+}
+
+func (r *RE) walker( index int ) bool {
+  index++
+  for oTextPos, oCatchIndex, oCatchIdIndex := r.txtPos, r.catchIndex, r.catchIdIndex;
+      r.asm[ index ].inst == asmPathEle
+      index, r.txtPos, r.catchIndex, r.catchIdIndex = r.asm[ index ].close, oTextPos, oCatchIndex, oCatchIdIndex {
+    if r.trekking( index + 1 ) { return true }
+  }
+
+  return false
+}
+
+
+func (r *RE) looper( index int ) bool {
+  loops := 0
+
+  if (r.asm[ index ].re.mods & modNegative) > 0 {
+    for forward := 0; loops < r.asm[ index ].re.loopsMax && r.txtPos < r.txtLen && !r.match( index, r.txt[r.txtInit + r.txtPos:], &forward ); {
+      r.txtPos += utf8meter( r.txt[r.txtInit + r.txtPos:] )
+      loops++;
+    }
+  } else {
+    for forward := 0; loops < r.asm[ index ].re.loopsMax && r.txtPos < r.txtLen &&  r.match( index, r.txt[r.txtInit + r.txtPos:], &forward ); {
+      r.txtPos += forward
+      loops++;
+    }
+  }
+
+  if loops < r.asm[ index ].re.loopsMin { return false }
+  return true
+}
+
+func (r *RE) loopGroup( index int ) bool {
+  loops, textxtPos := 0, r.txtPos;
+
+  if (r.asm[ index ].re.mods & modNegative) > 0 {
+    for loops < r.asm[ index ].re.loopsMax && !r.trekking( index + 1 ) {
+      textxtPos++;
+      r.txtPos = textxtPos;
+      loops++;
+    }
+
+    r.txtPos = textxtPos;
+  } else {
+    for loops < r.asm[ index ].re.loopsMax && r.trekking( index + 1 ) {
+      loops++;
+    }
+  }
+
+  if loops < r.asm[ index ].re.loopsMin { return false  }
+  return true
+}
+
+
+func (r *RE) match( index int, txt string, forward *int ) bool {
+  switch r.asm[ index ].re.reType {
+  case rePoint  : *forward = utf8meter( txt );  return true
+  case reSet    : return r.matchSet    ( index, txt, forward )
+  case reBackref: return r.matchBackRef( &r.asm[ index ].re, txt, forward )
+  case reRangeab: return matchRange    ( &r.asm[ index ].re, txt, forward )
+  case reMeta   : return matchMeta     ( &r.asm[ index ].re, txt, forward )
+  default       : return matchText     ( &r.asm[ index ].re, txt, forward )
+  }
+}
+
+func matchText( rexp *reStruct, txt string, forward *int ) bool {
+  *forward = len(rexp.str)
+
+  if len(txt) < *forward { return false }
+
+  if (rexp.mods & modCommunism) > 0 {
+    return strnEqlCommunist( txt, rexp.str, *forward )
+  }
+
+  return txt[:*forward] == rexp.str
+}
+
+func matchRange( rexp *reStruct, txt string, forward *int ) bool {
+  *forward = 1
+  if (rexp.mods & modCommunism) > 0 {
+    chr := toLower( rune(txt[0]) )
+    return chr >= toLower( rune(rexp.str[ 0 ]) ) && chr <= toLower( rune(rexp.str[ 2 ]) )
+  }
+
+  return txt[0] >= rexp.str[ 0 ] && txt[0] <= rexp.str[ 2 ];
+}
+
+func matchMeta( rexp *reStruct, txt string, forward *int ) bool {
+  var f func( r rune ) bool
+  *forward = 1
+
+  switch rexp.str[1] {
+  case 'a' : return isAlpha( rune(txt[0]) )
+  case 'A' : f = isAlpha
+  case 'd' : return isDigit( rune(txt[0]) )
+  case 'D' : f = isDigit
+  case 'w' : return isAlnum( rune(txt[0]) )
+  case 'W' : f = isAlnum
+  case 's' : return isSpace( rune(txt[0]) )
+  case 'S' : f = isSpace
+  case 'b' : return isBlank( rune(txt[0]) )
+  case 'B' : f = isBlank
+  case '&' : if txt[0] < 128 { return false }
+    *forward = utf8meter( txt )
+    return true
+  default  : return txt[0] == rexp.str[1]
+  }
+
+  if f( rune(txt[0]) ) { return false }
+  *forward = utf8meter( txt )
+  return true
+}
+
+func (r *RE) matchSet( index int, txt string, forward *int ) (result bool) {
+  *forward = 1
+
+  for index++; !result && r.asm[ index ].inst != asmSetEnd; index++ {
+    switch r.asm[ index ].inst {
+    case asmRangeab, asmUTF8, asmMeta:
+      result = r.match( index, txt, forward )
+    default:
+      if (r.asm[ index ].re.mods & modCommunism)  > 0 {
+        result = findRuneCommunisto( r.asm[ index ].re.str, rune( txt[ 0 ] ) )
+      } else {
+        result = strnchr( r.asm[ index ].re.str, rune( txt[ 0 ] ) )
+      }
+    }
+
+    if result { return true }
+  }
+
+  return false
+}
+
+func (r *RE) matchBackRef( rexp *reStruct, txt string, forward *int ) bool {
+  backRefId    := aToi( rexp.str[1:] )
+  backRefIndex := r.lastIdCatch( backRefId )
+  strCatch     := r.GetCatch( backRefIndex )
+  *forward      = len(strCatch)
+
+  if strCatch == "" || len( txt ) < *forward || strCatch != txt[:*forward] { return false }
+
+  return true
+}
+
+func (r *RE) lastIdCatch( id int ) int {
+  for index := r.catchIndex - 1; index > 0; index-- {
+    if r.catches[ index ].id == id { return index }
+  }
+
+  return len(r.catches);
+}
+
+func (r *RE) openCatch() (index int) {
+  index = r.catchIndex
+
+  if r.catchIndex < len(r.catches) {
+    r.catches[index] = catchInfo{ r.txtInit + r.txtPos, r.txtInit + r.txtPos, r.catchIdIndex }
+  } else {
+    r.catches = append( r.catches, catchInfo{ r.txtInit + r.txtPos, r.txtInit + r.txtPos, r.catchIdIndex } )
+  }
+
+  r.catchIndex++
+  r.catchIdIndex++
+  return
+}
+
+func (r *RE) closeCatch( index int ){
+  if index < r.catchIndex {
+    r.catches[index].end = r.txtInit + r.txtPos
+  }
+}
+
+func (r *RE) Result  () int { return r.result }
+
+func (r *RE) TotCatch() int { return r.catchIndex - 1 }
+
+func (r *RE) GetCatch( index int ) string {
+  if index > 0 && index < r.catchIndex {
+    return r.txt[ r.catches[index].init : r.catches[index].end ]
+  }
+
+  return ""
+}
+
+func (r *RE) GpsCatch( index int ) int {
+  if index > 0 && index < r.catchIndex {
+    return r.catches[index].init
+  }
+
+  return 0
+}
+
+func (r *RE) LenCatch( index int ) int {
+  if index > 0 && index < r.catchIndex {
+    return r.catches[index].end - r.catches[index].init
+  }
+
+  return 0
+}
+
+func (r *RE) RplCatch( rplStr string, id int ) (result string) {
+  last := 0
+
+  for index := 1; index < r.catchIndex; index++ {
+    if r.catches[index].id == id {
+      result += r.txt[last:r.catches[index].init]
+      result += rplStr
+      last    = r.catches[index].end
+    }
+  }
+
+  if last < len(r.txt) { result += r.txt[last:] }
+
+  return string(result)
+}
+
+func (r *RE) PutCatch( pStr string ) (result string) {
+  for i := 0; i < len(pStr); {
+    if pStr[i] == '#' {
+      i++
+      if len(pStr[i:]) > 0 && pStr[i] == '#' {
+        i++
+        result += "#"
+      } else {
+        result += r.GetCatch( aToi( pStr[i:] ) )
+        i      += countCharDigits ( pStr[i:] )
+      }
+    } else { result += pStr[i:i+1]; i++ }
+  }
+
+  return
+}
